c49b55b51a14e21734557bf5b88d7acd9ec284fa
Xumaowu <17705621902@189.com>
2020-09-09 15:24:39 +0800

    这是我第一次提交

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-09 11:51:03
+ * @LastEditTime: 2020-09-09 11:51:58
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \babel-demo1\babel.config.js
+ */
+module.exports={
+    presets:[
+        '@babel/preset-env'
+    ]
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-09 11:45:03
+ * @LastEditTime: 2020-09-09 11:47:11
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \babel-demo1\index.js
+ */
+const fn=(a,b)=>a+b;
+const added=fn(1,2)
