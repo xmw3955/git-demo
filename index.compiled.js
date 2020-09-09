b69703e1486057ec14c8145d019244144884699e
Xumaowu <17705621902@189.com>
2020-09-09 22:39:18 +0800

    iii

---
+++ index.compiled.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/*
+ * @Author: your name
+ * @Date: 2020-09-09 11:45:03
+ * @LastEditTime: 2020-09-09 11:47:11
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \babel-demo1\index.js
+ */
+var fn = function fn(a, b) {
+  return a + b;
+};
+
+var added = fn(1, 2);
